698053f982ab61fcefec1c15aa7fc1bdbad197ee
Abdulrhman Ahmed <rookr1082@gmail.com>
2026-02-01 23:00:00 +0300

    DTOs FIle

---
+++ ApplictionTypeDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DTOs
+{
+    public class ApplictionTypeDTO
+    {
+        public int ApplictionTypeID { get; set; }
+        public string ApplictionTypeTitle { get; set; }
+        public decimal ApplictionTypeFees { get; set; }
+
+        public ApplictionTypeDTO(int ID, string Title, decimal Fees)
+        {
+            this.ApplictionTypeID = ID;
+            this.ApplictionTypeTitle = Title;
+            this.ApplictionTypeFees = Fees;
+        }
+
+        public ApplictionTypeDTO() { }
+
+    }
+}
---
+++ TestTypeDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DTOs
+{
+    public class TestTypeDTO
+    {
+        public enum enTestType { VisionTest = 1, WrittenTest = 2, StreetTest = 3 };
+        public enTestType TestTypeID { get; set; }
+        public string TestTypeTitle { get; set; }
+        public string TestTypeDescription { get; set; } // May be null
+        public decimal TestTypeFees { get; set; }
+
+        public TestTypeDTO(int ID, string Description, decimal Fees)
+        {
+            this.TestTypeID = (enTestType)ID;
+            this.TestTypeDescription = Description;
+            this.TestTypeFees = Fees;
+        }
+
+        public TestTypeDTO() { }
+    }
+}
